08cb02452226c481ebe58d51d9516ae19e2b48b2
jagbir <jagbir@cloudgenix.com>
2017-03-14 14:25:35 -0700

    Added zip file

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ MYDIR=$(cd $(dirname ${BASH_SOURCE[0]:-$0}); pwd)
 pushd ${MYDIR}
 [[ -d requests ]] || pip install -t ${MYDIR} requests
 [[ -d boto ]] || pip install -t ${MYDIR} boto
-zip -FS -r9 ealerts.zip ealerts.py requests boto --exclude \*.pyc
+TARGET="target"
+mkdir -p ${TARGET}
+zip -FS -r9 ${TARGET}/ealerts.zip ealerts.py requests boto --exclude \*.pyc
 MD5SUM=md5
 which md5 || MD5SUM=md5sum
-${MD5SUM} ealerts.zip
+MD5VAL=$(${MD5SUM} ${TARGET}/ealerts.zip | cut -d' ' -f1)
+echo ${MD5VAL} > ${TARGET}/ealerts.md5
 popd
